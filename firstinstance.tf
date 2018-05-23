fbd1d41faccfb0e410b8b0fba1174388ddd247c5
Marko in1t3r Pavlovic <marko.shiva.pavlovic@gmail.com>
2018-05-23 18:23:30 +0200

    Init

---
+++ firstinstance.tf
@@ -0,0 +1,45 @@
+#varaibles
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "private_key_path" {}
+variable "key_name" {
+  default="aws"
+}
+
+#providers
+
+provider "aws" {
+
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region = "us-east-2"
+}
+
+#resources
+resource "aws_key_pair" "mykeypair" {
+  key_name = "aws"
+  public_key = "${file("/home/in1t3r/.ssh/aws.pub")}"
+}
+resource "aws_instance" "nginx" {
+  ami = "ami-922914f7"
+  instance_type = "t2.micro"
+  key_name = "${var.key_name}"
+
+  connection {
+    user    = "ec2-user"
+    private_key = "${file(var.private_key_path)}"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+    "sudo yum install nginx -y",
+    "sudo service start nginx"
+    ]
+  }
+}
+
+#Output
+
+output "aws_instance_public_dns" {
+value = "${aws_instance.nginx.public_dns}"
+}
